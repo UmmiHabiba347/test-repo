e5599cbff7b7053454f4b115274bb5c37e5f97a6
UmmiHabiba347 <ummihabeeba9@gmail.com>
2024-10-15 14:26:09 -0700

    Added changes for ayesha03 branch

--- src/App.js
+++ src/App.js
@@ -6,22 +6,10 @@ import Navbar from './Components/Navbar';
 import Course1 from './View/Course1';
 import AboutUs from './View/Aboutus'
 import Dialog from './Components/SubmitDialog';
-import { FaSquareCheck } from "react-icons/fa6";
+
 
 function App() {
     
-  // const [ShowDialog, setShowDialog] = useState(false)
-  // const [DialogTitle, setDialogTitle] = useState('');
-  // const [DialogChildren, setDialogChildren] = useState(null);
-
-  // const handleModal = (targetModal)=>{
-  //   console.log(targetModal)
-  //   if(targetModal === 'dialog'){
-  //     setDialogTitle('')
-  //     setDialogChildren(<Dialog />)
-  //   }
-  //   setShowDialog(true)
-  // }
   
   return (
     <>
@@ -29,21 +17,12 @@ function App() {
 {/* <Navbar /> */}
 {/* <LandingPage /> */}
  <Course1 />
+ 
  {/* <AboutUs /> */}
 
 
 </div>
-{/* <button onClick={()=>handleModal('dialog')} className='text-[20px] rounded-lg py-4 px-8 font-bold'>submit</button>
 
-      <Dialog
-        show = {ShowDialog}
-        handleClose = {()=>setShowDialog(false)}
-        Title= {'Form Submitted Successfully'}
-        body = {<FaSquareCheck size={'100px'} color='#1D8C6E' />}
-        size={'sm'}
-        position={'top'}
-             
-      /> */}
  
     </>
   );
